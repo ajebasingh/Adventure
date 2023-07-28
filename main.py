6902cbab7d37914756b136857e6bf7bb886b903b
Andrew Jebasingh <andrew@jebtek.com>
2023-07-28 11:16:58 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+# Adventure text based game!
+import random
+
+power = 0
+life = 10
+
+
+def welcome(name):
+    """
+    This function will welcome the user, and provide game instructions
+    :param name:
+    :return:
+    """
+    print(f'Hi, {name}!  ')
+
+
+def perform_action(action):
+    if action == 1:
+        global power
+        power += random.randint(1, 3)
+    if action == 2:
+        global life
+        life -= random.randint(1, 3)
+
+
+# Run Adventure!
+if __name__ == '__main__':
+    welcome('PyCharm')
+    action = int(input("Enter an action: "))
+
+    while action != 0:
+        perform_action(action)
+        action = int(input("Enter an action: "))
+
+    print(f"Your final score: {power}")
+
