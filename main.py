4b4f0d9107eda9184ed77ef3538e6a076daad2bd
Konyay <140830838+Konyay@users.noreply.github.com>
2023-07-28 11:46:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,10 +19,15 @@ def perform_action(action):
         global power
         power += random.randint(1, 3)
         print("You've received the power: Immunity to Sarcasm... This sure looks useful!")
-    if action == 2:
+    elif action == 2:
         global life
         life -= random.randint(1, 3)
-    print("You have lost a heart.")
+        print("You have lost a heart.")
+    else:
+        1 == ("slot 1")
+        2 == ("slot 2")
+        3 == ("slot 3")
+        print("you can only use 1,2 and 3")
 
 # Run Adventure!
 if __name__ == '__main__':
